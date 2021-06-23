93867bc21165de062846b5b98ff5ae67580d4be5
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-06-23 12:41:55 -0300

    Roteamento

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,16 @@ import googleImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useHistory } from 'react-router-dom';
 
 export function Home() {
+
+    let history = useHistory();
+
+    function navigateToNewRoom() {
+        history.push('/rooms/new');
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -16,7 +24,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask logo" />
-                    <button className="create-room">
+                    <button onClick={navigateToNewRoom} className="create-room">
                         <img src={googleImg} alt="Logo do google" />
                         Crie sua sala com o Google
                     </button>
